cd3988354ceda9ab09e214095779ddf186462d6f
Eyrim <marfxedbot@gmail.com>
2025-04-16 00:54:31 +0100

    Add git add alias

--- git-scripts.sh
+++ git-scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 alias gs="git status";
+alias cgs="clear; git status";
+alias ga='git diff --name-only | fzf --sync --preview="git diff --color -- {}" | xargs git add';
 
 branch () {
     git branch --show-current;
