03151c9844f059199090f77fb9dbb69959f3289c
Eyrim <marfxedbot@gmail.com>
2025-04-16 00:55:15 +0100

    Add gp and make branch alias not func

--- git-scripts.sh
+++ git-scripts.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
 
 alias gs="git status";
+alias gp="git pull";
 alias cgs="clear; git status";
 alias ga='git diff --name-only | fzf --sync --preview="git diff --color -- {}" | xargs git add';
-
-branch () {
-    git branch --show-current;
-}
+alias branch="git branch --show-current";
 
